6225f9b78922cfd04841c302bbf7dabd4645a4b8
Aleksandr <aleksandr@asdfjackal.com>
2019-10-02 18:22:06 -0400

    Added multiple options to dropdown.

--- main.js
+++ main.js
@@ -5,8 +5,21 @@ literalSearch = function(word){
    chrome.tabs.create({url: url });
 };
 
+literalSearchBackground = function(word){
+   var query = word.selectionText;
+   var replaced = query.replace(' ', '+');
+   var url = 'https://www.google.com/search?q="' + replaced + '"';
+   chrome.tabs.create({url: url, active: false});
+}
+
 chrome.contextMenus.create({
 title: "Literal Search",
 contexts:["selection"],  // ContextType
 onclick: literalSearch // A callback function
+});
+
+chrome.contextMenus.create({
+title: "Literal Search in Background",
+contexts:["selection"],  // ContextType
+onclick: literalSearchBackground // A callback function
 });
